352ef0db4d5e5c1d04851f722563d47602fc5500
Piyush <piyush.2024@nst.rishihood.edu.in>
2025-02-16 23:15:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,11 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function getMSFromProperty(prop, el) {
-  return parseFloat(getComputedStyle(document.querySelector(el)).getPropertyValue(prop)) * 1000;
+  return (
+    parseFloat(
+      getComputedStyle(document.querySelector(el)).getPropertyValue(prop)
+    ) * 1000
+  );
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -64,13 +68,12 @@ document.addEventListener("DOMContentLoaded", function () {
       const height = section.offsetHeight;
 
       if (scrollPosition >= top && scrollPosition < top + height) {
-        navbarLinks.forEach(link => {
+        navbarLinks.forEach((link) => {
           link.classList.remove("active");
         });
         navbarLinks[index].classList.add("active");
       }
     });
-
   }
 
   window.addEventListener("scroll", highlightNavbarLink);
@@ -91,33 +94,30 @@ document.addEventListener("DOMContentLoaded", function () {
       const height = section.offsetHeight;
 
       if (scrollPosition >= top && scrollPosition < top + height) {
-        navbarLinks.forEach(link => {
+        navbarLinks.forEach((link) => {
           link.classList.remove("active");
         });
         navbarLinks[index].classList.add("active");
       }
     });
-
   }
 
   window.addEventListener("scroll", highlightNavbarLink);
   highlightNavbarLink();
 });
 
-
-
 // Wait for the DOM to be fully loaded
-document.addEventListener("DOMContentLoaded", function() {
-    // Hide the first preloader after a delay
-    setTimeout(function() {
-        document.querySelector('.first-preloader').style.display = 'none';
-        // Show the second preloader
-        document.querySelector('.second-preloader').style.display = 'block';
-    }, 4000); // Adjust the delay time as needed
+document.addEventListener("DOMContentLoaded", function () {
+  // Hide the first preloader after a delay
+  setTimeout(function () {
+    document.querySelector(".first-preloader").style.display = "none";
+    // Show the second preloader
+    document.querySelector(".second-preloader").style.display = "block";
+  }, 4000); // Adjust the delay time as needed
 
-    // Hide the entire preloader and show the website content after another delay
-    setTimeout(function() {
-        document.getElementById('preloader').style.display = 'none';
-        // Add any additional code here to show the website content or load other scripts
-    }, 9000); // Adjust the delay time as needed
+  // Hide the entire preloader and show the website content after another delay
+  setTimeout(function () {
+    document.getElementById("preloader").style.display = "none";
+    // Add any additional code here to show the website content or load other scripts
+  }, 9000); // Adjust the delay time as needed
 });
